4a699843fca42d8152e7d19b6e30135efe5ece13
tanish jain <tanishjain7566669375@gmail.com>
2026-06-01 22:33:43 +0530

    Initial commit

---
+++ src/main/java/com/hofo/HofoApplication.java
@@ -0,0 +1,13 @@
+package com.hofo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HofoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HofoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hofo/HofoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hofo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HofoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
